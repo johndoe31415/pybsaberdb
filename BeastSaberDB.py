4e4ff7d8b889da4083b912bff085bf335efdfef1
Johannes Bauer <joe@johannes-bauer.com>
2019-09-15 16:57:24 +0200

    More song details added
    Now can retrieve and search in database of songs as well.

--- BeastSaberDB.py
+++ BeastSaberDB.py
@@ -25,6 +25,13 @@ import json
 import time
 from lxml import etree
 from CachedRequests import CachedRequests
+from Song import Song
+
+def _dict_factory(cursor, row):
+	result = { }
+	for idx, col in enumerate(cursor.description):
+		result[col[0]] = row[idx]
+	return result
 
 class BeastSaberDB():
 	_URIS = {
@@ -38,6 +45,8 @@ class BeastSaberDB():
 		self._session = CachedRequests(fixed_headers = { "Accept": "application/json" }, minimum_gracetime_secs = 1.0, cache_failed_requests = False)
 		self._db = sqlite3.connect("beastsaber.sqlite3")
 		self._cursor = self._db.cursor()
+		self._dict_cursor = self._db.cursor()
+		self._dict_cursor.row_factory = _dict_factory
 
 		with contextlib.suppress(sqlite3.OperationalError):
 			self._cursor.execute("""
@@ -129,15 +138,47 @@ class BeastSaberDB():
 			"recommended":	recommended,
 		}
 
-	def fill_song_details(self, song_key):
+	def fill_song_details(self, song_key, verbose = False):
 		details = self.retrieve_song_details(song_key)
-		print(song_key, details)
+		if verbose:
+			print(song_key, details)
 		categories_json = json.dumps(sorted(list(details["categories"])))
-		self._cursor.execute("UPDATE songs SET metadata_update_timet = ?, difficulty_easy = ?, difficulty_normal = ?, difficulty_hard = ?, difficulty_expert = ?, difficulty_expertplus = ?, thumbs_up = ?, thumbs_down = ?, categories_json = ? WHERE song_key = ?;",
-				(time.time(), "easy" in details["difficulties"], "normal" in details["difficulties"], "hard" in details["difficulties"], "expert" in details["difficulties"], "expert+" in details["difficulties"], details["thumbs_up"], details["thumbs_down"], categories_json, song_key))
+		self._cursor.execute("UPDATE songs SET metadata_update_timet = ?, difficulty_easy = ?, difficulty_normal = ?, difficulty_hard = ?, difficulty_expert = ?, difficulty_expertplus = ?, thumbs_up = ?, thumbs_down = ?, recommended = ?, categories_json = ? WHERE song_key = ?;",
+				(time.time(), "easy" in details["difficulties"], "normal" in details["difficulties"], "hard" in details["difficulties"], "expert" in details["difficulties"], "expert+" in details["difficulties"], details["thumbs_up"], details["thumbs_down"], details["recommended"], categories_json, song_key))
 		self._db.commit()
+		return details
 
-	def fill_missing_song_details(self):
-		for row in self._cursor.execute("SELECT song_key FROM songs WHERE metadata_update_timet is NULL;").fetchall():
+	def fill_missing_song_details(self, verbose = False):
+		song_keys = self._cursor.execute("SELECT song_key FROM songs WHERE metadata_update_timet is NULL ORDER BY song_key ASC;").fetchall()
+		for (rid, row) in enumerate(song_keys):
 			song_key = row[0]
-			self.fill_song_details(song_key)
+			details = self.fill_song_details(song_key, verbose = False)
+			if verbose:
+				print("%5.1f%% (%d of %d): %s (%s)" % (rid / len(song_keys) * 100, rid, len(song_keys), song_key, str(details)))
+
+	def search_songs(self, must_have_difficulties = None, minimum_percentage = None, minimum_votes = None, must_be_recommended = False):
+		where = set()
+		where.add("metadata_update_timet IS NOT NULL")
+		if minimum_votes is not None:
+			where.add("(thumbs_up + thumbs_down > %d)" % (minimum_votes))
+		if minimum_percentage is not None:
+			where.add("(thumbs_up / (thumbs_up + thumbs_down)) > %.3f" % (minimum_percentage / 100))
+		if must_have_difficulties is not None:
+			if "easy" in must_have_difficulties:
+				where.add("difficulty_easy = 1")
+			if "normal" in must_have_difficulties:
+				where.add("difficulty_normal = 1")
+			if "hard" in must_have_difficulties:
+				where.add("difficulty_hard = 1")
+			if "expert" in must_have_difficulties:
+				where.add("difficulty_expert = 1")
+			if "expert+" in must_have_difficulties:
+				where.add("difficulty_expertplus = 1")
+		if must_be_recommended:
+			where.add("recommended = 1")
+
+		where_clause = "WHERE " + (" AND ".join(sorted("(%s)" % (clause) for clause in where)))
+		sql = "SELECT song_key, level_author, title, hash, difficulty_easy, difficulty_normal, difficulty_hard, difficulty_expert, difficulty_expertplus, recommended, thumbs_up, thumbs_down, categories_json FROM songs %s;" % (where_clause)
+		for rowdict in self._dict_cursor.execute(sql).fetchall():
+			song = Song.from_rowdict(rowdict)
+			print(song)
---
+++ Song.py
@@ -0,0 +1,100 @@
+#	pybsaberdb - Python interface to BeastSaber database
+#	Copyright (C) 2019-2019 Johannes Bauer
+#
+#	This file is part of pybsaberdb.
+#
+#	pybsaberdb is free software; you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation; this program is ONLY licensed under
+#	version 3 of the License, later versions are explicitly excluded.
+#
+#	pybsaberdb is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with pybsaberdb; if not, write to the Free Software
+#	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+#	Johannes Bauer <JohannesBauer@gmx.de>
+
+import json
+
+class Song():
+	def __init__(self, song_key, level_author, title, hash, difficulties, recommended, thumbs_up, thumbs_down, categories):
+		self._song_key = song_key
+		self._level_author = level_author
+		self._title = title
+		self._hash = hash
+		self._difficulties = difficulties
+		self._recommended = recommended
+		self._thumbs_up = thumbs_up
+		self._thumbs_down = thumbs_down
+		self._categories = categories
+
+	@property
+	def song_key(self):
+		return self._song_key
+
+	@property
+	def level_author(self):
+		return self._level_author
+
+	@property
+	def title(self):
+		return self._title
+
+	@property
+	def hash(self):
+		return self._hash
+
+	@property
+	def difficulties(self):
+		return self._difficulties
+
+	@property
+	def recommended(self):
+		return self._recommended
+
+	@property
+	def thumbs_up(self):
+		return self._thumbs_up
+
+	@property
+	def thumbs_down(self):
+		return self._thumbs_down
+
+	@property
+	def categories(self):
+		return self._categories
+
+	@property
+	def total_votes(self):
+		return self.thumbs_up + self.thumbs_down
+
+	@property
+	def percentage(self):
+		if self.total_votes == 0:
+			return 0
+		else:
+			return self.thumbs_up / self.total_votes * 100
+
+	@classmethod
+	def from_rowdict(cls, rowdict):
+		difficulties = set()
+		if rowdict["difficulty_easy"]:
+			difficulties.add("easy")
+		if rowdict["difficulty_normal"]:
+			difficulties.add("normal")
+		if rowdict["difficulty_hard"]:
+			difficulties.add("hard")
+		if rowdict["difficulty_expert"]:
+			difficulties.add("expert")
+		if rowdict["difficulty_expertplus"]:
+			difficulties.add("expert+")
+		categories = set(json.loads(rowdict["categories_json"]))
+		return cls(song_key = rowdict["song_key"], level_author = rowdict["level_author"], title = rowdict["title"], hash = rowdict["hash"], difficulties = difficulties, recommended = bool(rowdict["recommended"]), thumbs_up = rowdict["thumbs_up"], thumbs_down = rowdict["thumbs_down"], categories = categories)
+
+	def __str__(self):
+		return "%s: %s (by %s) -- %.1f%% (%d votes, %d up, %d down)" % (self.song_key, self.title, self.level_author, self.percentage, self.total_votes, self.thumbs_up, self.thumbs_down)
